ccbec15c57b16986c3e72766353574dae4341bef
Suet Kee <suet.kee@softwire.com>
2025-01-21 15:08:56 +0000

    Part 3

--- EmailExtraction.js
+++ EmailExtraction.js
@@ -7,9 +7,30 @@
         return;
       }
 
-      const re = /[a-zA-Z.'_%+-]+@softwire.com/g
+      //const re = /[a-zA-Z.'_%+-]+@softwire.com/g
+      const re = /[a-zA-Z.'_%+-]+@[a-zA-Z]+\.[a-zA-Z.]{2,}/g
+  
       let matches = data.match(re);
-      console.log(matches.length);
+
+      let dictionary = {};
+
+      for (let i = 0; i < matches.length; i ++) {
+        let email = matches[i];
+        // katrina_dwain@softwire.com
+        let atIndex = email.indexOf('@');
+        let domain = email.slice(atIndex + 1);
+
+        if (dictionary[domain]) {
+          dictionary[domain] = dictionary[domain] + 1;
+        } else {
+          dictionary[domain] = 1
+        }
+
+      }
+
+
+      console.log(dictionary);
+
       
 });
      
